8d6c10051dc915f564002eb11dc427d06b038962
Abbos29 <abbosvaliev2003@gmail.com>
2025-04-10 16:33:53 +0500

    feat: add footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import s from './Footer.module.scss'
+import Container from '@/components/ui/Container/Container'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className={s.footer}>
+            <Container>
+                <div className={s.wrapper}>
+                    <div className={s.item}>
+                        <h3>Sign up and save with us</h3>
+                        <p>
+                            Get 10% off your first order when you subscribe to receive exclusive offers and new product announcements!
+                        </p>
+                        <button>Sign up</button>
+                    </div>
+                    <div className={s.item}>
+                        <h3>Customer Service</h3>
+                        <Link href="/help-center">Help Center</Link>
+                        <Link href="/shipping-delivery">Shipping & Delivery</Link>
+                        <Link href="/return-policy">Return Policy</Link>
+                        <Link href="/contact-us">Contact Us</Link>
+                    </div>
+                    <div className={s.item}>
+                        <h3>Resources</h3>
+                        <Link href="/coupons-promotions">Coupons & Promotions</Link>
+                        <Link href="/military-first-responders">Military & First Responders</Link>
+                    </div>
+                    <div className={s.item}>
+                        <h3>Company Info</h3>
+                        <Link href="/classes-workshops">Classes & Workshops</Link>
+                        <Link href="/about-us">About Us</Link>
+                        <Link href="/retail-store">Retail Store</Link>
+                        <Link href="/gift-cards">Gift Cards</Link>
+                    </div>
+                </div>
+            </Container>
+        </footer>
     )
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/reset.css";
 import "@/styles/globals.css";
 import TopBar from "@/components/ui/TopBar/TopBar";
 import Header from "@/components/layout/Header/Header";
+import Footer from "@/components/layout/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
       <TopBar />
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
