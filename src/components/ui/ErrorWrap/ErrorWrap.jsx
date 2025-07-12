4f9d513ff051892cb032f925dd8c6f404ca229eb
Abbos29 <abbosvaliev2003@gmail.com>
2025-07-12 21:32:58 +0500

    fix: error page

--- src/components/ui/ErrorWrap/ErrorWrap.jsx
+++ src/components/ui/ErrorWrap/ErrorWrap.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import s from './ErrorWrap.module.scss'
-import Container from '../Container/Container'
-import Link from 'next/link'
+import React from 'react';
+import s from './ErrorWrap.module.scss';
+import Container from '../Container/Container';
+import Link from 'next/link';
+import { FaTools } from 'react-icons/fa';
 
 const ErrorWrap = () => {
     return (
-        <>
-            <section className={s.errorwrap}>
-                <Container>
-                    <div className={s.wrapper}>
-                        <h1 className={s.title}>404</h1>
-                        <p className={s.text}>Siden ble ikke funnet</p>
-                        <Link href="/" className={s.link}>Tilbake til forsiden</Link>
-                    </div>
-                </Container>
-            </section>
-        </>
-    )
-}
+        <section className={s.errorwrap}>
+            <Container>
+                <div className={s.wrapper}>
+                    <FaTools fontSize="3rem" className={s.icon} />
+                    <h1 className={s.title}>Denne siden er under utvikling</h1>
+                    <p className={s.text}>Vi jobber med å gjøre den tilgjengelig så snart som mulig.</p>
+                    <Link href="/" className={s.link}>
+                        Tilbake til forsiden
+                    </Link>
+                </div>
+            </Container>
+        </section>
+    );
+};
 
-export default ErrorWrap
+export default ErrorWrap;
